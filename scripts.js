65a361f48033a77e1b5ef07f9f7dda9cf4a6ae83
gauravsharma180 <157041996+gauravsharma180@users.noreply.github.com>
2024-08-24 01:54:54 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,4 @@
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const addToCartButtons = document.querySelectorAll('.product button');
-
-//     addToCartButtons.forEach(button => {
-//         button.addEventListener('click', () => {
-//             alert('Product added to cart!');
-//         });
-//     });
-// });
 
 
 document.addEventListener('DOMContentLoaded', () => {
